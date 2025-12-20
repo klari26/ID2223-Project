e552fa0c5014241b818faa538f72c6ffac8c41ae
Klara <kvarga@kth.se>
2025-12-20 11:35:51 +0000

    Get warning historical data and historical weather data (5 years)

---
+++ locations.py
@@ -0,0 +1,48 @@
+# Taken from google maps
+resort_locations = {
+    "Narvik Ski Resort": (68.47375268157305, 17.429653561603484),
+    "Strandafjellet Skisenter": (62.399663364662885, 6.899584741440454),
+    "Skimore Oslo": (60.163287696205884, 10.743011831983127),
+    "Norefjell": (60.22504485319862, 9.552881597110982),
+    "Hafjell": (61.29805193508792, 10.403023844075184),
+    "Kvitfjell ski resort": (61.510160873788585, 10.0875276091632),
+    "Drammen ski center": (59.823446420964814, 10.13062504674089),
+    "Voss Resort Fjellheisar": (60.66679721890424, 6.4163768467408895),
+    "Myrkdalen Fjellandsby": (60.91609240579905, 6.531733285654033),
+    "Nedre fjellheisstasjon Narvik": (68.48097085721196, 17.40440897292097),
+    "Skimore Kongsberg": (59.726222161412714, 9.644141538014194),
+    "Eikedalen Ski Center AS": (60.48535825973864, 5.921807746740899),
+    "Hemsedal Skisenter": (60.910916204614885, 8.494636927952046),
+    "Rauland Skisenter": (59.81912239767133, 8.161511965529717),
+    "Vrådal Panorama": (59.36593432520996, 8.430676989660377),
+    "Galdhøpiggen Summer Ski Centre": (61.74877912234984, 8.373904910498657),
+    "SkiGeilo": (60.56868628949149, 8.144531465087947),
+    "Sauda Ski Centre": (59.65347720745357, 6.223121107043404),
+    "Hovden Alpinsenter": (59.59545094991248, 7.326028486460486),
+    "Bjorli Ski": (62.37332305036026, 8.277476069954428)
+}
+
+
+# to get historical data
+region_ids  = {
+    "Narvik Ski Resort": 3015,
+    "Strandafjellet Skisenter": 3024,
+    "Skimore Oslo": 3042,
+    "Norefjell": 3041,
+    "Hafjell": 3042,
+    "Kvitfjell ski resort": 3042,
+    "Drammen ski center": 3041,
+    "Voss Resort Fjellheisar": 3031,
+    "Myrkdalen Fjellandsby": 3031,
+    "Nedre fjellheisstasjon Narvik": 3015,
+    "Skimore Kongsberg": 3041,
+    "Eikedalen Ski Center AS": 3031,
+    "Hemsedal Skisenter": 3032,
+    "Rauland Skisenter": 3035,
+    "Vrådal Panorama": 3039,
+    "Galdhøpiggen Summer Ski Centre": 3028,
+    "SkiGeilo": 3032,
+    "Sauda Ski Centre": 3034,
+    "Hovden Alpinsenter": 3035,
+    "Bjorli Ski": 3023
+    }
---
+++ util.py
@@ -0,0 +1,95 @@
+import os
+import datetime
+import time
+import requests
+import pandas as pd
+import openmeteo_requests
+import requests_cache
+import json
+from retry_requests import retry
+from datetime import datetime, timedelta
+
+def get_historical_weather(location, start_date, end_date, longitude, latitude):
+
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Choose this
+    daily_variables = [
+        "temperature_2m_mean",
+        "precipitation_sum",
+        "rain_sum",
+        "snowfall_sum",
+        "wind_speed_10m_max",
+        "wind_direction_10m_dominant",
+    ]
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": start_date,
+        "end_date": end_date,
+        "daily": daily_variables
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    # Build date range
+    daily_data = {
+        "date": pd.date_range(
+            start=pd.to_datetime(daily.Time(), unit="s"),
+            end=pd.to_datetime(daily.TimeEnd(), unit="s"),
+            freq=pd.Timedelta(seconds=daily.Interval()),
+            inclusive="left"
+        )
+    }
+
+    # Extract each variable dynamically based on index
+    for idx, var_name in enumerate(daily_variables):
+        daily_data[var_name] = daily.Variables(idx).ValuesAsNumpy()
+
+    # Convert to DataFrame
+    df = pd.DataFrame(daily_data).dropna()
+    df['location'] = location
+    return df
+
+def get_warning_data(start_date, end_date, lat, lon, lang=2):
+    url = (
+        f"https://api01.nve.no/hydrology/forecast/avalanche/v6.3.0/api/"
+        f"AvalancheWarningByCoordinates/Simple/"
+        f"{lat}/{lon}/{lang}/{start_date}/{end_date}"
+    )
+
+    headers = {"User-Agent": "avalanche-research-app"}
+
+    print(url)
+
+    try:
+        r = requests.get(url, headers=headers, timeout=20)
+        r.raise_for_status()
+        return r.json()
+    except Exception as e:
+        print(f"API error {start_date}–{end_date}: {e}")
+        return []
+
+def date_chunks(start_date, end_date, chunk_days=60):
+    start = datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.strptime(end_date, "%Y-%m-%d")
+
+    while start < end:
+        chunk_end = min(start + timedelta(days=chunk_days), end)
+        yield start.strftime("%Y-%m-%d"), chunk_end.strftime("%Y-%m-%d")
+        start = chunk_end + timedelta(days=1)
