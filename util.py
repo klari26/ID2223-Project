c70b01f6377483f282b3ef26cb1bd4d304044f9a
Klara <kvarga@kth.se>
2025-12-23 15:15:24 +0000

    add daily pipeline and yaml

--- util.py
+++ util.py
@@ -41,10 +41,10 @@ def get_historical_weather(location, start_date, end_date, longitude, latitude):
 
     # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    # print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    # print(f"Elevation {response.Elevation()} m asl")
+    # print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    # print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
     # Process daily data. The order of variables needs to be the same as requested.
     daily = response.Daily()
@@ -67,6 +67,59 @@ def get_historical_weather(location, start_date, end_date, longitude, latitude):
     df['location'] = location
     return df
 
+def get_hourly_weather_forecast(location, longitude, latitude):
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    hourly_variables = [
+        "temperature_2m",
+        "precipitation",
+        "rain",
+        "snowfall",
+        "wind_speed_10m",
+        "wind_direction_10m"
+    ]
+
+    url = "https://api.open-meteo.com/v1/ecmwf"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": hourly_variables
+    }
+
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
+    hourly = response.Hourly()
+
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
+    hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+    hourly_snowfall = hourly.Variables(3).ValuesAsNumpy()
+    hourly_wind_speed_10m = hourly.Variables(4).ValuesAsNumpy()
+    hourly_wind_direction_10m = hourly.Variables(5).ValuesAsNumpy()
+
+    hourly_data = {
+        "date": pd.date_range(
+            start=pd.to_datetime(hourly.Time(), unit="s"),
+            end=pd.to_datetime(hourly.TimeEnd(), unit="s"),
+            freq=pd.Timedelta(seconds=hourly.Interval()),
+            inclusive="left"
+        )
+    }
+
+    hourly_data["temperature_2m_mean"] = hourly_temperature_2m
+    hourly_data["precipitation_sum"] = hourly_precipitation
+    hourly_data["rain_sum"] = hourly_rain
+    hourly_data["snowfall_sum"] = hourly_snowfall
+    hourly_data["wind_speed_10m_max"] = hourly_wind_speed_10m
+    hourly_data["wind_direction_10m_dominant"] = hourly_wind_direction_10m
+
+    df = pd.DataFrame(hourly_data).dropna()
+    df['location'] = location
+    return df
+
+
 def get_warning_data(start_date, end_date, lat, lon, lang=2):
     url = (
         f"https://api01.nve.no/hydrology/forecast/avalanche/v6.3.0/api/"
@@ -76,7 +129,7 @@ def get_warning_data(start_date, end_date, lat, lon, lang=2):
 
     headers = {"User-Agent": "avalanche-research-app"}
 
-    print(url)
+    # print(url)
 
     try:
         r = requests.get(url, headers=headers, timeout=20)
